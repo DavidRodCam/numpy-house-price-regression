793d680079dcf84d4287f444200e50440f844cf7
David Rodriguez Camacho <dajoroca64@gmail.com>
2026-08-05 15:46:25 -0600

    Step 5: append_column

--- model.py
+++ model.py
@@ -57,8 +57,10 @@ def make_ratio_feature(numerator, denominator, eps=1e-8):
     
     return numerator / (denominator + eps)
 
-# Step 5 - append_column (not yet solved)
-# TODO: implement
+# Step 5 - append_column
+def append_column(X, col):
+    
+    return np.column_stack([X, col])
 
 # Step 6 - one_hot_encode (not yet solved)
 # TODO: implement
