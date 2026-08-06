02824ed8b2bcb70a0503862d92b39b3d364ec2f3
David Rodriguez Camacho <dajoroca64@gmail.com>
2026-08-06 15:54:28 -0600

    Step 12: subset_xy

--- model.py
+++ model.py
@@ -119,8 +119,13 @@ def partition_indices(indices, train_ratio, val_ratio):
 
     return train_idx, val_idx, test_idx
 
-# Step 12 - subset_xy (not yet solved)
-# TODO: implement
+# Step 12 - subset_xy
+def subset_xy(X, y, indices):
+
+    X_sub = X[indices]
+    y_sub = y[indices]
+
+    return X_sub, y_sub
 
 # Step 13 - ols_fit (not yet solved)
 # TODO: implement
